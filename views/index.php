2b700c34c85f6e0c66025b6daac9410c8155c8b1
Lucas Cardoso <lucas@straube.co>
2020-09-10 19:54:44 -0300

    Added the basics for using the Bootstrap.

--- views/index.php
+++ views/index.php
@@ -2,11 +2,21 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+            integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <title>Madeira Madeira</title>
     </head>
     <body>
-        <div class="">
-            <h4>Cálculo de números</h4>
+        <div class="container">
+            <form method="POST" action="index.php">
+                <h3>Calculadora de ímpares</h3>
+                <p>Insira um número e receba uma somatoria de todos os números ímpares até o número escolhido!</p>
+                <div class="form-group">
+                    <label for="number">Digite um número</label>
+                    <input type="text" class="form-control" placeholder="Digite um número" name="number">
+                </div>
+                <button type="submit" name="button" class="btn btn-sm btn-primary">Calcular</button>
+            </form>
         </div>
     </body>
 </html>
