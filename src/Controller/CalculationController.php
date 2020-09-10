6645d0778477eab7c1ffad9240d9603cce047abe
Lucas Cardoso <lucas@straube.co>
2020-09-10 20:40:04 -0300

    Added errors to controller.

---
+++ src/Controller/CalculationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controller;
+
+class CalculationController extends Controller
+{
+    public function show(array $errors = array())
+    {
+        include BASE_PATH . '/views/index.php';
+    }
+
+    /* creating array with validations */
+    public function validate(): array
+    {
+        $errors = array();
+
+        $number = trim($_POST['number']);
+
+        if(empty($number)) {
+            $errors['number_empty'] = 'O campo não pode estar vazio!';
+        }
+
+        if (!preg_match('/^[0-9]+$/i', $number)) {
+            $errors['number'] = 'Por favor, insira apenas números!';
+        }
+
+        return $errors;
+    }
+
+    public function process()
+    {
+    }
+}
