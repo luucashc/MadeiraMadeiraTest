0431c01e81fcbb6e73eaa514b9956ea48d9d7b82
Lucas Cardoso <lucas@straube.co>
2020-09-10 19:49:28 -0300

    Added IndexController to start communication.

---
+++ src/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function show(array $errors = [])
+    {
+        /* Show index */
+        include BASE_PATH . '/views/index.php';
+    }
+
+    public function validate(): array
+    {
+        return [];
+    }
+
+    public function process()
+    {
+    }
+}
