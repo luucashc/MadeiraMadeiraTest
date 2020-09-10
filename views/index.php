05e73c58d478d6fe3c711dfcadb431266dde40bc
Lucas Cardoso <lucas@straube.co>
2020-09-10 19:40:53 -0300

    Added views folder and index.

---
+++ views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Madeira Madeira</title>
+    </head>
+    <body>
+        <div class="">
+            <h4>Cálculo de números</h4>
+        </div>
+    </body>
+</html>
