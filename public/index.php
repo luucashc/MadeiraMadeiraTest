ac8a9ec622b73767783a0ef4f76493b76f4e884e
Lucas Cardoso <lucas@straube.co>
2020-09-10 19:34:52 -0300

    Made front controller call a controller class based on the current URL (route).

--- public/index.php
+++ public/index.php
@@ -12,3 +12,16 @@ $route = preg_replace('/^([a-z]+).*$/i', '$1', ltrim($_SERVER['REQUEST_URI'], '/
 if (empty($route)) {
     $route = 'index';
 }
+
+/* Assembling the class name with namespace */
+$class = 'Controller\\' . ucfirst($route) . 'Controller';
+
+/* If the class does not exist, it gives a 404 error (not found) */
+if (!class_exists($class)) {
+    echo '404 not found';
+    exit;
+}
+
+/* Creates a controller class and calls its index */
+$controller = new $class;
+$controller->index();
