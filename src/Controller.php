593bca75855a6dbe5ca55e9c8011fa14cc370a74
Lucas Cardoso <lucas@straube.co>
2020-09-10 19:30:24 -0300

    Created abstract class to server as base controller.

---
+++ src/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controller;
+
+abstract class Controller
+{
+    public function index()
+    {
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if($form_errors = $this->validate()) {
+                $this->show($form_errors);
+            }else {
+                $this->process();
+            }
+        }else {
+            $this->show();
+        }
+    }
+
+    /* forces these classes to contain these functions */
+    abstract public function validate(): array;
+
+    abstract public function show(array $errors);
+
+    abstract public function process();
+}
