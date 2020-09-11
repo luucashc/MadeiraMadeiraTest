f8625b4c87479e6b8e92ae7f9688445612ab489f
Lucas Cardoso <lucas@straube.co>
2020-09-10 21:30:29 -0300

    Function to summation odd numbers.

--- src/Model/Calculation.php
+++ src/Model/Calculation.php
@@ -2,10 +2,22 @@
 
 namespace Model;
 
+/**
+ * [Calculation description]
+ * 
+ */
 class Calculation
 {
     public static function calculation($number)
     {
-        return $number;
+        $summation = 0;
+
+        for ($result = 0; $result < $number; $result++) {
+            if ($result % 2 == 1) {
+                $summation = $summation + $result;
+            }
+        }
+
+        return $summation;
     }
 }
