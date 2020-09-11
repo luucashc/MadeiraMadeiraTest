6ac2c2bc7f4e3e01140505e94dc13ec5cb65ccb9
Lucas Cardoso <lucas@straube.co>
2020-09-11 16:46:15 -0300

    Test and documentation.

--- tests/MyTest.php
+++ tests/MyTest.php
@@ -2,8 +2,12 @@
 
 use PHPUnit\Framework\TestCase;
 
+use Model\Calculation;
+
 /**
- * [Test description]
+ * Unit test class.
+ *
+ * To test whether the calculation is matching the expected result value.
  *
  */
 class MyTest extends TestCase
@@ -11,6 +15,12 @@ class MyTest extends TestCase
     public function test()
     {
         // $this->assertTrue(true);
-        $this->assertArrayHasKey('1', ['1' => '2']);
+        $calculation = new Calculation();
+
+        $this->assertTrue($calculation->calculation(10) == 25);
+
+        $this->assertTrue($calculation->calculation(50) == 625);
+
+        $this->assertTrue($calculation->calculation(100) == 2500);
     }
 }
